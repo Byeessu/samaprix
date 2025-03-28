b0992e999d1852becfb0ba8142c48b1c26700330
user <user@users-MacBook-Pro.local>
2025-03-28 15:26:24 +0000

    Fichiers HTML/CSS/JS corrigés

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById("search").addEventListener("input", function() {
-    let filter = this.value.toLowerCase();
-    let items = document.querySelectorAll("#products li");
-    items.forEach(item => {
-        if (item.innerText.toLowerCase().includes(filter)) {
-            item.style.display = "";
-        } else {
-            item.style.display = "none";
-        }
-    });
+  let filter = this.value.toLowerCase();
+  let items = document.querySelectorAll("#products li");
+  items.forEach(item => {
+    if (item.innerText.toLowerCase().includes(filter)) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
+    }
+  });
 });
